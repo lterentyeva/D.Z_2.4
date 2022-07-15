b262e91a5b0b9af560f04687d2da7ceeaaac0b80
lterentyeva <lterentyeva@yandex.ru>
2022-07-16 01:35:35 +0300

    Add test

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -3,28 +3,41 @@ package ru.netology.web.data;
 import lombok.Value;
 
 public class DataHelper {
-  private DataHelper() {}
+    private DataHelper() {
+    }
 
-  @Value
-  public static class AuthInfo {
-    private String login;
-    private String password;
-  }
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
 
-  public static AuthInfo getAuthInfo() {
-    return new AuthInfo("vasya", "qwerty123");
-  }
+    public static AuthInfo getAuthInfo() {
 
-  public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-    return new AuthInfo("petya", "123qwerty");
-  }
+        return new AuthInfo("vasya", "qwerty123");
+    }
 
-  @Value
-  public static class VerificationCode {
-    private String code;
-  }
 
-  public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-    return new VerificationCode("12345");
-  }
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardNumber {
+        String cardNumber;
+    }
+
+    public static CardNumber getFirstCard() {
+        return new CardNumber("5559 0000 0000 0001");
+    }
+
+    public static CardNumber getSecondCard() {
+        return new CardNumber("5559 0000 0000 0002");
+    }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,25 +1,66 @@
 package ru.netology.web.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPageV2;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
 
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999", LoginPageV2.class);
+        var loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationInfo);
+    }
 
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var dashboardPage = new DashboardPage();
+        int firstBalance = dashboardPage.getFirstCardBalance();
+        int secondBalance = dashboardPage.getSecondCardBalance();
+        var moneyTransfer = dashboardPage.secondCardButton();
+        String total = "5000";
+        var card = DataHelper.getFirstCard();
+        moneyTransfer.shouldTransfer(total, card);
+
+        assertEquals(secondBalance + Integer.parseInt(total), dashboardPage.getSecondCardBalance());
+        assertEquals(firstBalance - Integer.parseInt(total), dashboardPage.getFirstCardBalance());
+
+    }
+
+    @Test
+    void shouldTransferFromSecondToFirst() {
+        var dashboardPage = new DashboardPage();
+        int firstBalance = dashboardPage.getFirstCardBalance();
+        int secondBalance = dashboardPage.getSecondCardBalance();
+        var moneyTransfer = dashboardPage.firstCardButton();
+        String total = "5000";
+        var card = DataHelper.getSecondCard();
+        moneyTransfer.shouldTransfer(total, card);
+
+        assertEquals(firstBalance + Integer.parseInt(total), dashboardPage.getFirstCardBalance());
+        assertEquals(secondBalance - Integer.parseInt(total), dashboardPage.getSecondCardBalance());
+
+    }
+
+    @Test
+    void shouldNotTransferOverBalance() {
+        var dashboardPage = new DashboardPage();
+        var moneyTransfer = dashboardPage.secondCardButton();
+        String total = "15000";
+        var card = DataHelper.getFirstCard();
+        moneyTransfer.shouldTransfer(total, card);
+        moneyTransfer.errorNotification();
+    }
 
 }
 
